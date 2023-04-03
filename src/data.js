bcbc0dba3cdab93435823f983337789af0118dd9
JosephVang099 <joseph-vang@outlook.com>
2023-04-03 12:40:56 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const sliderItems = [
+    {
+      id: 1,
+      title: "DISCOUNT",
+      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      title: "DISCOUNT",
+      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      title: "DISCOUNT",
+      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
+      bg: "fbf0f4",
+    },
+];
+
