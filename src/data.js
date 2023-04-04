787b0118918124223e1f932b9f52f8fe606edd18
JosephVang099 <joseph-vang@outlook.com>
2023-04-03 23:55:11 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,65 @@
-export const sliderItems = [
-    {
-      id: 1,
-      title: "DISCOUNT",
-      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
-      bg: "f5fafd",
-    },
-    {
-      id: 2,
-      title: "DISCOUNT",
-      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      title: "DISCOUNT",
-      desc: "SOJU ON SALE!! ONLY LIMITED TIME!!",
-      bg: "fbf0f4",
-    },
-];
+import GrapeSoju from './Img/grapesoju.jpeg';
+import HoneySoju from './Img/honeysoju.jpeg';
+import OriginalSoju from './Img/originalsoju.webp';
+import AmericanoSoju from './Img/americanosoju.jpeg';
+import LycheeSoju from './Img/lycheesoju.jpeg';
+import CitronSoju from './Img/citronsoju.jpeg';
+import MangoSoju from './Img/mangosoju.jpeg';
+import PineappleSoju from './Img/pineapplesoju.jpeg';
+import PomegranateSoju from './Img/pomegranatesoju.jpeg';
+import WatermelonSoju from './Img/watermelonsoju.jpeg';
+import AppleSoju from './Img/applesoju.jpeg';
+import PeachySoju from './Img/peachysoju.jpeg';
+
+
+export const popularProducts = [
+  {
+    id:1,
+    img: [GrapeSoju],
+  },
+  {
+    id:2,
+    img: [HoneySoju],
+  },
+  {
+    id:3,
+    img: [OriginalSoju],
+  },
+  {
+    id:4,
+    img: [AmericanoSoju],
+  },
+  {
+    id:5,
+    img: [LycheeSoju],
+  },
+  {
+    id:6,
+    img: [CitronSoju],
+  },
+  {
+    id:7,
+    img: [MangoSoju],
+  },
+  {
+    id:8,
+    img: [PineappleSoju],
+  },
+  {
+    id:9,
+    img: [PomegranateSoju],
+  },
+  {
+    id:10,
+    img: [WatermelonSoju],
+  },
+  {
+    id:11,
+    img: [AppleSoju],
+  },
+  {
+    id:12,
+    img: [PeachySoju],
+  },
+]
 
