dd1c374233cc46fb1f8e1483c329d2018c5c9b11
JosephVang099 <joseph-vang@outlook.com>
2023-04-04 19:55:10 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,41 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+// import Americano from './pages/products/Americano';
+// import Apple from './pages/products/Apple';
+// import Citron from './pages/products/Citron';
+// import Grape from './pages/products/Grape';
+// import Honey from './pages/products/Honey';
+// import Lychee from './pages/products/Lychee';
+// import Mango from './pages/products/Mango';
+// import Original from './pages/products/Original';
+// import Peachy from './pages/products/Peachy';
+// import Pineapple from './pages/products/Pineapple';
+// import Pomegranate from './pages/products/Pomegranate';
+// import Watermelon from './pages/products/Watermelon';
+
+
+
+
+// <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" pages={Home} />
+//         <Route path="/products/Americano" pages={Americano} />
+//         <Route path="/products/Apple" pages={Apple} />
+//         <Route path="/products/Citron" pages={Citron} />
+//         <Route path="/products/Grape" pages={Grape} />
+//         <Route path="/products/Honey" pages={Honey} />
+//         <Route path="/products/Lychee" pages={Lychee} />
+//         <Route path="/products/Mango" pages={Mango} />
+//         <Route path="/products/Original" pages={Original} />
+//         <Route path="/products/Peachy" pages={Peachy} />
+//         <Route path="/products/Pineapple" pages={Pineapple} />
+//         <Route path="/products/Pomegranate" pages={Pomegranate} />
+//         <Route path="/products/Watermelon" pages={Watermelon} />
+//         <Route path="/Home" pages={Home} />
+//         <Route path="/ContactUs" pages={ContactUs} />
+//       </Switch>
+//     </BrowserRouter>
