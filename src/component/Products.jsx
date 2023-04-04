424fe054b6b5b42cbc85d45c4b75c2af4f674e7d
JosephVang099 <joseph-vang@outlook.com>
2023-04-03 23:55:32 -0400

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import { popularProducts } from "../data";
 import Product from './Product';
 
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 const Products = () => {
   return (
     <Container>
-        <Product></Product>
+        {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
     </Container>
   )
 }
 
-export default Products
+export default Products;
