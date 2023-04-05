b6fb8623b6171e962f36df66f8b35f6c58a7be14
JosephVang099 <joseph-vang@outlook.com>
2023-04-05 15:42:42 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+import AppleSoju from './Img/applesoju.jpeg';
+import PeachySoju from './Img/peachysoju.jpeg';
+import OriginalSoju from './Img/originalsoju.webp';
+
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: [AppleSoju],
+      title: "DISCOUNT!!!",
+      desc: "SOJU ON SALE!! ONLY LIMITED TIME!! FROM MAY 1st TO JULY 20th",
+      bg: "fcf1ed",
+    },
+    {
+      id: 2,
+      img: [PeachySoju],
+      title: "PEACHY SOJU",
+      desc: "NEWEST FLAVOR IN TOWN!! DON'T MISS OUT!! ONLY FOR LIMITED TINE!!",
+      bg: "defff5",
+    },
+    {
+      id: 3,
+      img: [OriginalSoju],
+      title: "ORIGINAL SOJU",
+      desc: "TRADITIONALLY MADE",
+      bg: "fbf0f4",
+    },
+  ];
