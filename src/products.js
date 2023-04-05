cc4d4752e50291094a880841a4bd2ed84d7a0cec
JosephVang099 <joseph-vang@outlook.com>
2023-04-05 15:43:00 -0400

    Create products.js

---
+++ src/products.js
@@ -0,0 +1,87 @@
+import GrapeSoju from './Img/grapesoju.jpeg';
+import HoneySoju from './Img/honeysoju.jpeg';
+import OriginalSoju from './Img/originalsoju.webp';
+import AmericanoSoju from './Img/americanosoju.jpeg';
+import LycheeSoju from './Img/lycheesoju.jpeg';
+import CitronSoju from './Img/citronsoju.jpeg';
+import MangoSoju from './Img/mangosoju.jpeg';
+import PineappleSoju from './Img/pineapplesoju.jpeg';
+import PomegranateSoju from './Img/pomegranatesoju.jpeg';
+import WatermelonSoju from './Img/watermelonsoju.jpeg';
+import AppleSoju from './Img/applesoju.jpeg';
+import PeachySoju from './Img/peachysoju.jpeg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Grape Soju",
+        price: 10.99,
+        productImage: GrapeSoju,
+    },
+    {
+        id: 2,
+        productName: "Honey Soju",
+        price: 10.99,
+        productImage: HoneySoju,
+    },
+    {
+        id: 3,
+        productName: "Original Soju",
+        price: 8.99,
+        productImage: OriginalSoju,
+    },
+    {
+        id: 4,
+        productName: "Americano Soju",
+        price: 9.99,
+        productImage: AmericanoSoju,
+    },
+    {
+        id: 5,
+        productName: "Lychee Soju",
+        price: 11.99,
+        productImage: LycheeSoju,
+    },
+    {
+        id: 6,
+        productName: "Citron Soju",
+        price: 10.99,
+        productImage: CitronSoju,
+    },
+    {
+        id: 7,
+        productName: "Mango Soju",
+        price: 11.99,
+        productImage: MangoSoju,
+    },
+    {
+        id: 8,
+        productName: "Pineapple Soju",
+        price: 9.99,
+        productImage: PineappleSoju,
+    },
+    {
+        id: 9,
+        productName: "Pomegranate Soju",
+        price: 9.99,
+        productImage: PomegranateSoju,
+      },
+      {
+        id: 10,
+        productName: "Watermelon Soju",
+        price: 10.99,
+        productImage: WatermelonSoju,
+      },
+      {
+        id: 11,
+        productName: "Apple Soju",
+        price: 11.99,
+        productImage: AppleSoju,
+      },
+      {
+        id: 12,
+        productName: "Peachy Soju",
+        price: 9.99,
+        productImage: PeachySoju,
+      },
+  ];
