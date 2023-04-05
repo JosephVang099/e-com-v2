62dd7ca878daa51dd5500fa43eaa15a452514594
JosephVang099 <joseph-vang@outlook.com>
2023-04-05 15:42:44 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// MYSQL
+const express = require('express');
+const {getAllProducts} = require("./dbFiles/dbQueries");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async function (request, response){
+
+  try{
+    const [results] = await getAllProducts()
+    response.send({success: true, results})
+  }
+  catch(error){
+    response.status(500).send({
+      success: false,
+      error: "This didn't work",
+    })
+  }
+})
+
+app.listen(5001);
